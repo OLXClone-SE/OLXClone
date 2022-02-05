bcfaf5eb3593e2d1c8762df647ea4ead8d9640f3
Satya-Irrinki <sirrinki@ufl.edu>
2022-02-04 19:07:00 -0500

    OTP functionality implementation

--- workspaces/client/src/ForgotPasswordComponent/ForgotPasswordComponent.tsx
+++ workspaces/client/src/ForgotPasswordComponent/ForgotPasswordComponent.tsx
@@ -6,7 +6,6 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Logo from '../Logo.png';
@@ -36,10 +35,16 @@ function validateEmail(email:any){
           /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 }
+function validateOTP(otp:any){
+  return (otp)
+      .match(
+        /^[0-9\b]+$/
+      );
+}
   
   
   export default function ForgotPwd() {
-    const initialValues = {emailFormatError:"",checkEmailError:false};
+    const initialValues = {emailFormatError:"",checkEmailError:false,showOtp:'none',otpFormatError:"",checkOTPError:false,isValid:false};
       const [forgotPwdData, setData] = useState(initialValues)
       const styles = {
         helper: {
@@ -53,15 +58,39 @@ function validateEmail(email:any){
       const data = new FormData(event.currentTarget);
       const email = data.get('email');
       let emailError = "";
+      let otpError="";
       let checkEmail = false;
-      let validEmail = true;
+      let checkOTP=false;
+      let validEmail = 'none';
       if(!validateEmail(email)){
         emailError = "Please enter a valid email address";
         checkEmail = true;
+        validEmail = 'none'
+      }
+      else{
+        validEmail = ''
       }
   
       if(emailError!=null)
-        setData({emailFormatError:emailError,checkEmailError:checkEmail});
+        setData({emailFormatError:emailError,checkEmailError:checkEmail,showOtp:validEmail,otpFormatError:otpError,checkOTPError:checkOTP,isValid:false});
+    };
+    const handleSubmitOtp = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      const otp = data.get('otp');
+      let otpError="";
+      let checkOTP=false;
+      let validEmail = '';
+      let isValid = false
+      if(!validateOTP(otp)){
+        otpError = "Please enter a valid 4 digit OTP";
+        checkOTP = true;
+      }
+      else{
+        isValid=true;
+      }
+      if(otpError!=null)
+        setData({emailFormatError:"",checkEmailError:false,showOtp:validEmail,otpFormatError:otpError,checkOTPError:checkOTP,isValid:isValid});
     };
   return (
     <ThemeProvider theme={theme}>
@@ -76,11 +105,12 @@ function validateEmail(email:any){
           }}
         >
           <Avatar sx={{ height: '70px', width: '170px' }} alt="OLX CLONE" src={Logo} />
-          <Typography  variant="subtitle1" sx={{ mb: 1 }}> 
-                Please enter your registered email address. You will receive an OTP via email.
-          </Typography>  
-          <Box component="form"  onSubmit={handleSubmit} sx={{ mt: 1, width: '40ch' }}>
-            <Grid container spacing={1}>
+           
+          <Box component="form"  onSubmit={handleSubmit} sx={{display: forgotPwdData.showOtp === '' ? 'none' : '', width: '40ch' }}>
+            <Typography  variant="subtitle1" sx={{mt: 1}}> 
+                An OTP will be sent to your registered Email ID.
+            </Typography> 
+            <Grid container >
               <Grid item xs={12}>
                 <TextField
                   margin="normal"
@@ -93,18 +123,44 @@ function validateEmail(email:any){
                   helperText= {forgotPwdData.emailFormatError}
                   FormHelperTextProps={{ style: styles.helper }}
                   error={forgotPwdData.checkEmailError}
-                  autoFocus
                 />
               </Grid>
-            </Grid>
-            <Button
+              <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 2, mb: 2 }}
-            >
+              sx={{display: forgotPwdData.showOtp === '' ? 'none' : '', mt: 2, mb: 2 }}
+              >
               Get OTP
-            </Button>
+              </Button>
+              </Grid>
+          </Box>
+          <Box component="form"  onSubmit={handleSubmitOtp} sx={{display: forgotPwdData.showOtp === '' ? '' : 'none', width: '40ch' }}>
+              <Typography  variant="subtitle1" sx={{mt: 1}}> 
+                OTP has been sent to your registered Email ID. Please enter your 4 digit OTP below.
+              </Typography>  
+              <Grid container >
+              <Grid item xs={12} sx={{display: forgotPwdData.showOtp === '' ? '' : 'none'}}>
+                <TextField
+                  required
+                  margin="normal"
+                  fullWidth
+                  id="otp"
+                  label="Enter OTP"
+                  name="otp"
+                  helperText= {forgotPwdData.otpFormatError}
+                  FormHelperTextProps={{ style: styles.helper }}
+                />
+              </Grid>
+              <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              href={forgotPwdData.isValid ? '/reset' : '#'}
+              >
+              Validate OTP
+              </Button>
+            </Grid>
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/" variant="body2">
