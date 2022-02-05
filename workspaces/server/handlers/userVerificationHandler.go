0a712511b4e4da234cd4ddd2b63428044b6cc077
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-04 23:06:21 -0500

    fixed

--- workspaces/server/handlers/userVerificationHandler.go
+++ workspaces/server/handlers/userVerificationHandler.go
@@ -15,7 +15,7 @@ type mailVerificationResponse struct {
 type userVerificationRequestBody struct {
 	Mailid   string `json:"mailid"`
 	Password string `json:"password"`
-	Action 	 string	`json:"action"`
+	Action   string `json:"action"`
 }
 
 func createMailVerificationResponse(otpsent bool) []byte {
@@ -31,13 +31,17 @@ func verifyMail(writer http.ResponseWriter, userVerificationReqBodyObject userVe
 	userInfo := models.User{}
 	res := db.Table(userInfo.TableName()).Where(fmt.Sprintf("%s = ?", constants.MAIL_ID), userVerificationReqBodyObject.Mailid).Select(constants.MAIL_ID).Scan(&userInfo)
 	success := false
-	if((res.RowsAffected == 0 && userVerificationReqBodyObject.Action == constants.ACTION_SIGNUP)  ||  (res.RowsAffected != 0 && userVerificationReqBodyObject.Action == constants.ACTION_RESET_PASSWORD)) {
+	if (res.RowsAffected == 0 && userVerificationReqBodyObject.Action == constants.ACTION_SIGNUP) || (res.RowsAffected != 0 && userVerificationReqBodyObject.Action == constants.ACTION_RESET_PASSWORD) {
 		otp := utilities.SendOtp(userVerificationReqBodyObject.Mailid)
+		success = res.RowsAffected != 0 || (res.RowsAffected == 0 && userVerificationReqBodyObject.Action == constants.ACTION_SIGNUP)
 		user := models.VerifyUser{}
 		user.Mailid = userVerificationReqBodyObject.Mailid
 		user.OTP = otp
-		res := db.Table(user.TableName()).Create(&user)
-		success = res.RowsAffected != 0
+		if res.RowsAffected != 0 {
+			db.Table(user.TableName()).Create(&user)
+		} else {
+			db.Table(user.TableName()).Where(fmt.Sprintf("%s = ?", constants.MAIL_ID), userVerificationReqBodyObject.Mailid).Update("otp", otp)
+		}
 	}
 	mailVerificationObject := createMailVerificationResponse(success)
 	if mailVerificationObject != nil {
