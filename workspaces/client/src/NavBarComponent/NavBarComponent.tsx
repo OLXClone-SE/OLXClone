7a880be972cfcff59e5ab4c1e01d935e1b6f417c
jayachandragunturu <gunturujayachandra@gmail.com>
2022-02-09 18:31:37 -0500

    Added navigation bar component

---
+++ workspaces/client/src/NavBarComponent/NavBarComponent.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+export function NavBarComponent() {
+    return <>
+        <Navbar className="color-nav" expand="lg">
+            <Container className="justify-content-end">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-end" activeKey="/home">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <NavDropdown title="Categories" className="categories">
+                            <NavDropdown.Item href="#action/3.1">Category 1</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Category 2</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Category 3</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Category 4</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Category 5</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Category 6</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Nav className="justify-content-end" activeKey="/home">
+                    <Nav.Item>
+                        <NavDropdown title="Profile" id="basic-nav-dropdown" className="justify-content-end">
+                            <NavDropdown.Item href="/profile">Edit Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav.Item>
+                </Nav>
+            </Container>
+        </Navbar>
+    </>
+}
