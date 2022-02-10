84a923f009a727c368d09b05b4ad08e68ea82594
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-10 15:15:39 -0500

    added provider and created store

--- workspaces/client/src/Store/store.ts
+++ workspaces/client/src/Store/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer  from '../ReduxSlices/CounterSlice'
 
 export const store = configureStore({
   reducer: {
+    "counter" : counterReducer,
   },
 })
 
