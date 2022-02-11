7890cfc58de6d944d04f592280b49eef7a4a6699
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-11 12:20:09 -0500

    added dummy component to test the setup

--- workspaces/client/src/ReduxSlices/CounterSlice.ts
+++ workspaces/client/src/ReduxSlices/CounterSlice.ts
@@ -1,32 +1,29 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../Store/store'
-
 interface CounterState {
-  value: number
+    value: number
 }
 
 const initialState: CounterState = {
-  value: 0,
+    value: 0,
 }
 
 export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            console.log("hello")
+            state.value += 1
+        },
+        decrement: (state) => {
+            state.value -= 1
+        },
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.value += action.payload
+        },
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
-  },
 })
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
-
-export const selectCount = (state: RootState) => state.counter.value
 
 export default counterSlice.reducer
