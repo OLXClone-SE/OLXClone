db044af3e1975b40c4d84690e6e4567af8f9f733
maneesh reddy <martinmaneesh123@gmail.com>
2022-02-11 19:10:47 -0500

    add test case to test otp utilities on delete record

--- workspaces/server/utilities/otpUtilities_test.go
+++ workspaces/server/utilities/otpUtilities_test.go
@@ -1,6 +1,9 @@
 package utilities
 
 import (
+	"fmt"
+	"server/constants"
+	"server/models"
 	"testing"
 )
 
@@ -9,4 +12,22 @@ func TestGenerateOtp(t *testing.T) {
 	if got == 0 {
 		t.Errorf("not able to generate otp")
 	}
+}
+
+func TestDeleteRecord(t *testing.T) {
+    userInfo := models.VerifyUser{
+        Mailid: "newmail@gmail.com",
+        OTP:    -1,
+    }
+    db := GetDBInstance()
+    DeleteRecord(userInfo.Mailid, userInfo)
+    res := db.Table(userInfo.TableName()).Create(&userInfo)
+    if res.RowsAffected == 0 {
+        t.Errorf("failed to insert record")
+    }
+    DeleteRecord(userInfo.Mailid, userInfo)
+    res = db.Table(userInfo.TableName()).Where(fmt.Sprintf("%s = ?", constants.MAIL_ID), userInfo.Mailid)
+    if res.RowsAffected != 0 {
+        t.Errorf("record not deleted")
+    }
 }
