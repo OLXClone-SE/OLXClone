c4765323974f4abbe528c5563496aa87137b5b03
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-13 14:12:10 -0500

    added matcher utility

--- workspaces/server/utilities/hashingUtilities.go
+++ workspaces/server/utilities/hashingUtilities.go
@@ -6,3 +6,11 @@ func GetHashString(inputString string) string {
 	hash, _ := bcrypt.GenerateFromPassword([]byte(inputString), bcrypt.DefaultCost)
 	return string(hash)
 }
+
+func MatchHash(hash string, inputString string) bool {
+	errf := bcrypt.CompareHashAndPassword([]byte(hash), []byte(inputString))
+	if errf != nil && errf == bcrypt.ErrMismatchedHashAndPassword {
+		return false
+	}
+	return true
+}
