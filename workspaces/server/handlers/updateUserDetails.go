5387eee7e2ed1249f4a6807427a22a61b839bdfa
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-17 16:15:44 -0500

    added update user handler

--- workspaces/server/handlers/updateUserDetails.go
+++ workspaces/server/handlers/updateUserDetails.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
 	"server/constants"
 	"server/models"
 	"server/utilities"
@@ -24,4 +25,14 @@ func updateUserDetails(reqBodyObject models.UpdtableUserDetails) bool {
 	userInfo := models.User{}
 	res := db.Table(userInfo.TableName()).Where(fmt.Sprintf("%s = ?", constants.MAIL_ID), reqBodyObject.Mailid).UpdateColumn(constants.FirstName, reqBodyObject.Fname).UpdateColumn(constants.LastName, reqBodyObject.Lname).UpdateColumn(constants.Mobile, reqBodyObject.Phone)
 	return res.RowsAffected != 0
+}
+
+func UpdateUserDetailsHandler(writer http.ResponseWriter, request *http.Request) {
+	var updateUserDetailsReqBodyObject models.UpdtableUserDetails
+	utilities.ParseRequestBody(request, &updateUserDetailsReqBodyObject)
+	success := updateUserDetails(updateUserDetailsReqBodyObject)
+	updateUserDetailsResponse := createUpdateUserDetailsResponse(success)
+	if updateUserDetailsResponse != nil {
+		utilities.WriteJsonResponse(writer, http.StatusOK, updateUserDetailsResponse)
+	}
 }
