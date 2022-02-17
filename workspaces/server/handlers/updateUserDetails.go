b30ad2d12c2202c7568413c0b767cb09e59de408
venkata-sai-samanth-kommi <sai12999.k@gmail.com>
2022-02-17 15:35:43 -0500

    added logic to update user details in db

--- workspaces/server/handlers/updateUserDetails.go
+++ workspaces/server/handlers/updateUserDetails.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"fmt"
+	"server/constants"
+	"server/models"
 	"server/utilities"
 )
 
@@ -14,4 +17,11 @@ func createUpdateUserDetailsResponse(success bool) []byte {
 	}
 	updateUserDetailsResponse := utilities.ToJson(response)
 	return updateUserDetailsResponse
+}
+
+func updateUserDetails(reqBodyObject models.UpdtableUserDetails) bool {
+	db := utilities.GetDBInstance()
+	userInfo := models.User{}
+	res := db.Table(userInfo.TableName()).Where(fmt.Sprintf("%s = ?", constants.MAIL_ID), reqBodyObject.Mailid).UpdateColumn(constants.FirstName, reqBodyObject.Fname).UpdateColumn(constants.LastName, reqBodyObject.Lname).UpdateColumn(constants.Mobile, reqBodyObject.Phone)
+	return res.RowsAffected != 0
 }
