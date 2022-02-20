1e15c508976fde1eaa56ba2d01e03a644f301376
maneesh reddy <martinmaneesh123@gmail.com>
2022-02-19 19:10:47 -0500

    add test case to test otp utilities on verify otp

--- workspaces/server/utilities/otpUtilities_test.go
+++ workspaces/server/utilities/otpUtilities_test.go
@@ -38,3 +38,26 @@ func TestSendOtp(t *testing.T) {
         t.Errorf("not able to generate otp")
     }
 }
+
+func TestVerifyOTP(t *testing.T) {
+    userInfo := models.VerifyUser{
+        Mailid: "newmail@gmail.com",
+        OTP:    1234,
+    }
+    db := GetDBInstance()
+    DeleteRecord(userInfo.Mailid, userInfo)
+    res := db.Table(userInfo.TableName()).Create(&userInfo)
+    if res.RowsAffected == 0 {
+        t.Errorf("failed to insert record")
+    }
+    success := VerifyOTP(1234, userInfo.Mailid)
+    if !success {
+        t.Errorf("record not deleted")
+    }
+
+    success = VerifyOTP(4567, userInfo.Mailid)
+    if success {
+        t.Errorf("otp validation is not working")
+    }
+    DeleteRecord(userInfo.Mailid, userInfo)
+}
