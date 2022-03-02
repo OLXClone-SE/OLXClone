5dbb8bf88c32c9b9ab986b8b80a542a1545de19b
maneesh reddy <martinmaneesh123@gmail.com>
2022-03-01 19:10:47 -0500

    add test case to test hash string matching

--- workspaces/server/utilities/hashingUtilities_test.go
+++ workspaces/server/utilities/hashingUtilities_test.go
@@ -10,3 +10,15 @@ func TestGetHashString(t *testing.T) {
 		t.Errorf("length of the hash should not be zero")
 	}
 }
+
+func TestMatchHash(t *testing.T) {
+	hash := GetHashString("something")
+	got := MatchHash(hash, "something")
+	if !got {
+		t.Errorf("hash should match with password")
+	}
+	got = MatchHash(hash, "some")
+	if got {
+		t.Errorf("hash should not match with password")
+	}
+}
