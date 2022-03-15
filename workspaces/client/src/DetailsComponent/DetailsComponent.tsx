6b15125e2f8ad12a566bd910a672164e0012b519
Satya-Irrinki <sirrinki@ufl.edu>
2022-03-15 18:30:45 -0400

    Product Details implementation

--- workspaces/client/src/DetailsComponent/DetailsComponent.tsx
+++ workspaces/client/src/DetailsComponent/DetailsComponent.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavBarComponent } from "../NavBarComponent/NavBarComponent";
+import { Container } from "react-bootstrap";
+import { Copyright } from "../CopyrightComponent/CopyrightComponent";
+import { useAppSelector } from "../Store/hooks";
+import { RootState } from "../Store/store";
+import { useDispatch } from "react-redux";
+import { fetchUserProfile, saveUserProfile } from "../ReduxActions/UserProfileActions";
+import { updateProfileDetails } from "../ReduxSlices/UserProfileSlice";
+import { UserProfile } from "../Types/user";
 import Typography from '@mui/material/Typography';
 
 export function DetailsComponent() {
     return (
-        <Typography gutterBottom variant="h5" component="div">
-                    Product Details Here
-        </Typography>
+        <React.Fragment>
+            <Typography gutterBottom variant="h5" component="div">
+                Product Details Here
+            </Typography>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </React.Fragment>
+        
     );
 }
 export default DetailsComponent;
