93ea56a82b92a2ebf3aa53072a9df7e98e8ff061
maneesh reddy <martinmaneesh123@gmail.com>
2022-03-16 00:46:52 -0400

    OLXClone-133: resolve errors in mailUtilities tests

--- workspaces/server/utilities/mailUtility_test.go
+++ workspaces/server/utilities/mailUtility_test.go
@@ -1,11 +1,18 @@
 package utilities
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestSendMail(t *testing.T) {
+	t.Setenv("frommail", "seolxclone@gmail.com")
+	t.Setenv("smtpsocket", "587")
+	t.Setenv("smtpserver", "smtp.gmail.com")
+	t.Setenv("mailid", "seolxclone@gmail.com")
+	t.Setenv("mailpassword", "Qwerty@123")
 	got := SendMail("some@gmail.com", "", "")
+	fmt.Println(got)
 	if !got {
 		t.Errorf("not able to send mail")
 	}
