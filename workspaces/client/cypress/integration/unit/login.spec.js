f05404b67446758a15a43ced61b0c4daf27d0774
jayachandragunturu <gunturujayachandra@gmail.com>
2022-03-15 23:30:31 -0400

    Test for validating navigation to login page

--- workspaces/client/cypress/integration/unit/login.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Navigating to login page', () => {
-    it('Visits the login page on entering website URL', () => {
-      cy.visit('localhost:3000');
-      cy.location('pathname').should('eq', '/')
-    })
-  })
