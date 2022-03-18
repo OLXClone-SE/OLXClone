f4fb0d3af145288c3a1f98389828a4c9224bcc91
jayachandragunturu <gunturujayachandra@gmail.com>
2022-03-17 23:46:52 -0400

    Updated tests by mocking login validation

--- workspaces/client/cypress/integration/unit/categoryVerify.spec.js
+++ workspaces/client/cypress/integration/unit/categoryVerify.spec.js
@@ -1,35 +1,15 @@
+import {generateName,generatePassword} from '../../support/utils';
+
 describe("Test", function () {
   it("sign up", function () {
-    cy.visit("http://localhost:3000");
-    cy.get('input[name="mailid"]').type(generateName() + "@gmail.com");
-    cy.get('input[name="password"]').type(password());
-    cy.get("button").contains("Sign In").click();
-    cy.location("pathname").should("eq", "/home");
+    cy.visit('localhost:3000');
+    cy.get('input[name="mailid"]').type(generateName()+'@gmail.com');
+    cy.get('input[name="password"]').type(generatePassword());
+    cy.get('button').contains('Sign In').click();
+    cy.intercept('POST','http://localhost:4000/login',{"approved": true});
+    cy.intercept('POST','/getProducts',{"Products":[]});
+    cy.location('pathname').should('eq', '/home')
     cy.get("a").contains("Categories").click();
     cy.get(".dropdown-menu").children().should("have.length", 6);
   });
-  function generateName() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (var i = 0; i < 5; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  }
-  function password() {
-    var text = "";
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var LowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var specialCharacters = "!@#$%^&*";
-    var numbers = "1234567890";
-    for (var i = 0; i < 3; i++) {
-      text += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
-      text += LowerCase.charAt(Math.floor(Math.random() * LowerCase.length));
-      text += specialCharacters.charAt(
-        Math.floor(Math.random() * specialCharacters.length)
-      );
-      text += numbers.charAt(Math.floor(Math.random() * numbers.length));
-    }
-    return text;
-  }
 });
