b5c17645cf131e254457a5e07f8a3560a1d09a42
jayachandragunturu <gunturujayachandra@gmail.com>
2022-03-18 23:47:45 -0400

    Added Custom Login Command

--- workspaces/client/cypress/support/commands.js
+++ workspaces/client/cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import {generateName,generatePassword} from './utils';
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('localhost:3000');
+    cy.get('input[name="mailid"]').type(generateName()+'@gmail.com');
+    cy.get('input[name="password"]').type(generatePassword());
+    cy.get('button').contains('Sign In').click();
+    cy.intercept('POST','http://localhost:4000/login',{"approved": true});
+    cy.location('pathname').should('eq', '/home')
+  })
