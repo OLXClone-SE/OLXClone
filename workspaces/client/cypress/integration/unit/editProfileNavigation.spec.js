70935f0537a3876d76ce67fdb9a94d5bf5708b36
jayachandragunturu <gunturujayachandra@gmail.com>
2022-03-19 23:55:22 -0400

    Updated login action with custom command

--- workspaces/client/cypress/integration/unit/editProfileNavigation.spec.js
+++ workspaces/client/cypress/integration/unit/editProfileNavigation.spec.js
@@ -1,16 +1,11 @@
-import {generateName,generatePassword} from '../../support/utils';
-
+beforeEach(()=>{
+    cy.login()
+    })
+    
 describe('Test', function(){
 
     it('sign up', function(){
-
-        cy.visit('localhost:3000');
-        cy.get('input[name="mailid"]').type(generateName()+'@gmail.com');
-        cy.get('input[name="password"]').type(generatePassword());
-        cy.get('button').contains('Sign In').click();
-        cy.intercept('POST','http://localhost:4000/login',{"approved": true});
         cy.intercept('POST','/getProducts',{"Products":[]});
-        cy.location('pathname').should('eq', '/home')
         cy.get('a').contains('Profile').click();
         cy.get('.dropdown-menu').children().should('have.length',2);
 
