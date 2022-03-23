5e85d0a1bb7cf52b1e475c281bbd67c77cc622c8
maneesh reddy <martinmaneesh123@gmail.com>
2022-03-22 22:01:47 -0400

    OLXClone-133: add api test for user verification api in reset password action

---
+++ workspaces/server/handlers/userVerificationHandler_test.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestUserVerificationHandler(t *testing.T) {
+	// Set necessary environment variables
+	t.Setenv("dbhost", "localhost");
+	t.Setenv("dbuname", "postgres");
+	t.Setenv("dbpass", "Kvmr@1106");
+	t.Setenv("dbname", "postgres");
+	t.Setenv("dbport", "5432");
+
+	var jsonStr = []byte(`{"mailid":"abcd@gmail.com","password":"efgh@4567","action":"resetPassword"}`)
+
+	req, err := http.NewRequest("POST", "/signup", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UserVerificationHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := true;
+	var response mailVerificationResponse;
+	json.Unmarshal(rr.Body.Bytes(), &response);
+	if response.OtpSent != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			response.OtpSent, expected)
+	}
+}
