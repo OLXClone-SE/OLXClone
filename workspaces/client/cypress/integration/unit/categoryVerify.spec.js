2818fd33c7a6c989379dad50ec522a51f3fa1c1f
jayachandragunturu <gunturujayachandra@gmail.com>
2022-03-23 18:26:17 -0400

    Updated test descriptions

--- workspaces/client/cypress/integration/unit/categoryVerify.spec.js
+++ workspaces/client/cypress/integration/unit/categoryVerify.spec.js
@@ -2,8 +2,8 @@ beforeEach(()=>{
 cy.login()
 })
 
-describe("Test", function () {
-  it("sign up", function () {
+describe("Navigate to category dropdown", function () {
+  it("Categories should popup on clicking category drop down", function () {
     cy.intercept('POST','/getProducts',{"Products":[]});
     cy.get("a").contains("Categories").click();
     cy.get(".dropdown-menu").children().should("have.length", 6);
