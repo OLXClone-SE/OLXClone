badfcbf669a82f7c91079bc5f7f36895cc2376ea
maneesh reddy <martinmaneesh123@gmail.com>
2022-03-26 21:47:06 -0400

    OLXClone-133: add test for signup api handler

---
+++ workspaces/server/handlers/signupHandler_test.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestSignupHandler(t *testing.T) {
+	var jsonStr = []byte(`{"mailid":"abcd@gmail.com","fname":"abcd","lname":"efgh","password":"abcd@1234","otp":"1234","phone":"+1234567890"}`)
+
+	req, err := http.NewRequest("POST", "/signup", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(SignupHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := true;
+	var response loginResponse;
+	json.Unmarshal(rr.Body.Bytes(), &response);
+	if response.Approved != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			response.Approved, expected)
+	}
+}
