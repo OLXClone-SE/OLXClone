4338afc1cb4a505284fa0d01d368ca5b64032103
Satya-Irrinki <sirrinki@ufl.edu>
2022-04-08 12:30:45 -0400

    home page session management

--- workspaces/client/src/HomePageComponent/HomePageComponent.tsx
+++ workspaces/client/src/HomePageComponent/HomePageComponent.tsx
@@ -1,45 +1,54 @@
 import React, { useEffect } from "react";
 import { NavBarComponent } from "../NavBarComponent/NavBarComponent";
-import {
-  Container,
-} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { SearchComponent } from "../SearchComponent/SearchComponent";
 import { CardComponent } from "../CardComponent/CardComponent";
 import { Copyright } from "../CopyrightComponent/CopyrightComponent";
 import { useAppSelector } from "../Store/hooks";
 import { RootState, store } from "../Store/store";
-import { Navigate } from "react-router-dom";
 import LoaderComponent from "../LoaderComponent/LoaderComponent";
 import { useDispatch } from "react-redux";
 import { getProducts } from "../ReduxActions/ProductActions";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export function HomepageComponent() {
-
-  const { approved, pending } = useAppSelector((root: RootState) => root.LoginSlice);
-  const { products } = useAppSelector((root: RootState) => root.ProductSlice)
-  const searchWord = useAppSelector((root: RootState) => root.SearchSlice.value)
+  const navigate = useNavigate();
+  if (
+    !document.cookie.split(";").find((row) => row.startsWith("token="))?.length
+  ) {
+    console.log("not here");
+    navigate("/");
+  }
+  const { approved, pending } = useAppSelector(
+    (root: RootState) => root.LoginSlice
+  );
+  const { products } = useAppSelector((root: RootState) => root.ProductSlice);
+  const searchWord = useAppSelector(
+    (root: RootState) => root.SearchSlice.value
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (store.getState().ProductSlice.products == null)
-      dispatch(getProducts({ mailid: "" }))
-  })
+      dispatch(getProducts({ mailid: "" }));
+  });
 
   const matchPercent = (product: any, searchWord: any) => {
-    if (searchWord.length === 0) return 100
-    const name = product.productname
-    console.log(name, searchWord)
-    let intersection = name.split("").filter((x: any) => searchWord.split("").includes(x));
-    console.log(intersection)
-    console.log((intersection.length * 100 / name.length))
+    if (searchWord.length === 0) return 100;
+    const name = product.productname;
+    console.log(name, searchWord);
+    let intersection = name
+      .split("")
+      .filter((x: any) => searchWord.split("").includes(x));
+    console.log(intersection);
+    console.log((intersection.length * 100) / name.length);
     return (intersection.length / name.length) * 100;
-  }
+  };
 
-  return (
-    approved ?
-      <React.Fragment>
-        <style type="text/css">
-          {`
+  return approved ? (
+    <React.Fragment>
+      <style type="text/css">
+        {`
     .cards-view {
         display: flex;
         flex-direction: row;
@@ -48,22 +57,35 @@ export function HomepageComponent() {
         flex-flow: row wrap;
     }
     `}
-        </style>
-        <NavBarComponent />
-        <Container>
-          <SearchComponent />
-        </Container>
-        <Container className="cards-view">
-          {products?.filter(product => matchPercent(product, searchWord) >= 50).map(product => <CardComponent
-            path={product?.path}
-            name={product?.productname}
-            desc={product?.description}
-            price={product?.price}
-          />)}
-        </Container>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </React.Fragment>
-      : <>{pending ? <LoaderComponent /> : <><Navigate replace to="/" /></>}</>
+      </style>
+      <NavBarComponent />
+      <Container>
+        <SearchComponent />
+      </Container>
+      <Container className="cards-view">
+        {products
+          ?.filter((product) => matchPercent(product, searchWord) >= 50)
+          .map((product) => (
+            <CardComponent
+              path={product?.path}
+              name={product?.productname}
+              desc={product?.description}
+              price={product?.price}
+            />
+          ))}
+      </Container>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </React.Fragment>
+  ) : (
+    <>
+      {pending ? (
+        <LoaderComponent />
+      ) : (
+        <>
+          <Navigate replace to="/" />
+        </>
+      )}
+    </>
   );
 }
 
