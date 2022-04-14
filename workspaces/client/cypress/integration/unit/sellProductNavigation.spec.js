1d9096ca57319084767568c2932ddc2cb037b93b
jayachandragunturu <gunturujayachandra@gmail.com>
2022-04-13 20:25:16 -0400

    OLXCLONE-Test case for user able to sell product

--- workspaces/client/cypress/integration/unit/sellProductNavigation.spec.js
+++ workspaces/client/cypress/integration/unit/sellProductNavigation.spec.js
@@ -1,3 +1,5 @@
+import {generateName,generatePrice} from '../../support/utils';
+
 beforeEach(()=>{
     cy.login()
     })
@@ -9,3 +11,18 @@ describe("Navigagte to sell production page", function () {
         cy.location('pathname').should('eq', '/sell')
     });
 });
+
+
+describe("User should be able to enter product details and submit", function () {
+    it("Should fill the product details and submit", function () {
+        cy.intercept('POST','/getProducts',{"Products":[]});
+        cy.get("a").contains("Sell").click();
+        cy.location('pathname').should('eq', '/sell')
+        cy.get('input[name="productname"]').type(generateName());
+        cy.get('input[name="price"]').type(generatePrice());
+        cy.get('button').contains('Sell Product').click();
+        cy.location('pathname').should('eq', '/sell')
+    });
+});
+
+
